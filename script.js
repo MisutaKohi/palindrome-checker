ae088aa4bb5f08fd32461f0cf1452ddbd1fbbdf3
Ryan Morgan <RyanOnGithub@gmail.com>
2024-01-22 21:08:37 -0800

    final commit

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ const checkPalindromeButton = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
 function isPalindrome(str) {
-    
+    if (str === '') {
+        alert("Please input a value.");
+        return;
+    }
+
+    const originalInput = str;
+
+    resultDiv.replaceChildren();
+
+    let lowerCaseStr = str.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+    const resultString = `<strong>${originalInput}</strong> ${lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'} a palindrome`;
+
+    const pTag = document.createElement('p');
+    pTag.className = 'display-output';
+    pTag.innerHTML = resultString;
+
+    resultDiv.style.display = 'block';
+    resultDiv.appendChild(pTag);
 }
 
 checkPalindromeButton.addEventListener("click", () => {
